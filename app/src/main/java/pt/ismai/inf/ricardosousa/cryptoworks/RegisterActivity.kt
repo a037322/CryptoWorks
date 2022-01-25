bc8bfd129b1e7f42c8a2213baa6a46e93cf7cabd
a037322 <a037322@ismai.pt>
2022-01-25 19:21:10 +0000

    page Register started

---
+++ app/src/main/java/pt/ismai/inf/ricardosousa/cryptoworks/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package pt.ismai.inf.ricardosousa.cryptoworks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
